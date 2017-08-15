b17830211c7ab6bd198130696710adabd5dc6165
Pearla Papiernik <pearlapb@users.noreply.github.com>
2017-08-15 14:15:49 +0200

    googlemaps api

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/Chart.js';
+import GoogleMap from '../components/GoogleMap.js'
 // GET THE WEATHER DATA:
 // 1. import connect function
 // 2. Define function mapStateToProps
@@ -13,10 +14,11 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const { lon, lat } = cityData.city.coord;
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
                 <td><Chart data={temps} color='orange' unit='K' /></td>
                 <td><Chart data={pressures} color='blue' unit='hPa' /></td>
                 <td><Chart data={humidities} color='green' unit='%' /></td>
